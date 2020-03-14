dbefa2196a5b2851658bd1f43cf15fa56d2bd2e7
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-03-14 15:55:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<?php
-	session_start();
 
-	if(!isset($_SESSION['username'])){
-		$_SESSION['msg'] = 'You must login first';
-		header('location: login.php');
-	}
-
-	if(isset($_GET['logout'])){
-		session_destroy();
-		unset($_SESSION['username']);
-		header('location: login.php');
-	}
-?>
 <html>
  <head>
   <title>ระบบจัดการสินค้า</title>
