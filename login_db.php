ad33747783dbbbe116f5183e5cadc6b0474a66c7
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-03-14 17:46:16 +0700

    Update login_db.php

--- login_db.php
+++ login_db.php
@@ -2,35 +2,45 @@
     include("db.php");
     session_start();
 
-    $errors= array();
+    $message = "";
+    // $errors= array();
 
-    if(isset($_POST['login_user'])){
-        $username = mysqli_real_escape_string($connect,$_POST['username']);
-        $password = mysqli_real_escape_string($connect,$_POST['password']);
-
-        if(empty($username)){
-            array_push($errors, "Username is required");
-        }
-
-        if(empty($password)){
-            array_push($errors, "Password is required");
-        }
-
-        if(count($errors) == 0){
-            $password = $password;
-            $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
-            $result = mysqli_query($connect, $query);
-
-            if(mysqli_num_rows($result) == 1){
-                $_SESSION['username'] = $username;
-                $_SESSION['success'] = "You are now logged in";
-                header("location: index.php");
-            } else {
-                array_push($errors, "Wrong username/password combination");
-                $_SESSION['error'] = "Wrong username or password try again";
-                header("location: login.php");
-            }
-        }
-    }
+    try  
+ {    
+      $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+      if(isset($_POST["login_user"]))  
+      {  
+           if(empty($_POST["username"]) || empty($_POST["password"]))  
+           {  
+                $message = '<label>All fields are required</label>';  
+           }  
+           else  
+           {  
+                $query = "SELECT * FROM user WHERE username = :username AND password = :password";  
+                $statement = $connection->prepare($query);  
+                $statement->execute(  
+                     array(  
+                          'username'     =>     $_POST["username"],  
+                          'password'     =>     $_POST["password"]  
+                     )  
+                );  
+                $count = $statement->rowCount();  
+                if($count > 0)  
+                {  
+                     $_SESSION["username"] = $_POST["username"];  
+                     header("location:index.php");  
+                }  
+                else  
+                {  
+                     $message = '<label>Wrong Data</label>';  
+                     header("location: login.php");
+                }  
+           }  
+      }  
+ }  
+ catch(PDOException $error)  
+ {  
+      $message = $error->getMessage();  
+ }
 ?>
 
