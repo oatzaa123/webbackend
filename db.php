87662604c3474de98da58af89a0681c81513e32f
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-03-14 15:20:43 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,6 @@
 <?php
 
-$username = 'b04445289daf70';
-$password = 'f9c7c57b';
-$connection = new PDO( 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_6876a6ae8580ca8', $username, $password );
+$connection = new PDO( 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_6876a6ae8580ca8', 'b04445289daf70', 'f9c7c57b' );
 
 ?>
 
