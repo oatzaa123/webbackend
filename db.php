bcf0e0535a800008555e607445281b93b633b8ae
oatzaa123 <53205207+oatzaa123@users.noreply.github.com>
2020-03-14 15:01:49 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
-$username = 'root';
-$password = '';
-$connection = new PDO( 'mysql:host=localhost;dbname=flexmessage', $username, $password );
+$username = 'b04445289daf70';
+$password = 'f9c7c57b';
+$connection = new PDO( 'mysql:host=us-cdbr-iron-east-04.cleardb.net;dbname=heroku_6876a6ae8580ca8', $username, $password );
 
 ?>
 
